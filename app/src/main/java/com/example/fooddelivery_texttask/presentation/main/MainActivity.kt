4c6d1c0bd19a26856b1b8958087ec8c50ba19c60
Olya Tukanova <daytobekey@gmail.com>
2022-10-14 23:24:19 +0600

    main activity create and view main screen

--- app/src/main/java/com/example/fooddelivery_texttask/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/fooddelivery_texttask/presentation/main/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.fooddelivery_texttask.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.compose.setContent
 import com.example.fooddelivery_texttask.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContent {
             MainScreen()
         }
